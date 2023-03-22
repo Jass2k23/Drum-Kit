3fa5b7b9df59ebfdbbe1f3c8a6f01a13dbf5e4ca
Jaskaran Singh <94312817+Jass2k23@users.noreply.github.com>
2023-03-22 10:25:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+
+
+// ANIMATION
+function buttonAnimation(keyClicked){
+    document.querySelector("." + keyClicked).classList.add("pressed");
+
+    setTimeout(function(){
+        document.querySelector("." + keyClicked).classList.remove("pressed");
+    }, 150)
+}
+
+// CHECKS IF ANY DRUM BUTTON WAS CLICKED, ALSO WHICH WAS CLICKED
+for(var i=0;i<7;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonCLicked = this.innerHTML;
+        soundSelector(buttonCLicked);
+        buttonAnimation(buttonCLicked);
+    });
+}
+
+
+// SOUND SELECTOR
+function soundSelector (key){
+    switch(key){
+    
+        case "w":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+    
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+    
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+    
+        case "k":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+    
+        case "l":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break; 
+            
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    soundSelector(e.key);
+    buttonAnimation(e.key);
+})
+
